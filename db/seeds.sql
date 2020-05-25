747c49ebe892cc112acf352d97ab5ef504647208
Ryan Parker <ryansparker@gmail.com>
2020-05-25 16:05:18 -0400

    added db folder and schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Curry-Spiced Lamb Burger');
+INSERT INTO burgers (burger_name) VALUES ('Japanese Burger with Wasabi Ketchup');
+INSERT INTO burgers (burger_name) VALUES ('Bacon-Topped Meatloaf Burger');
