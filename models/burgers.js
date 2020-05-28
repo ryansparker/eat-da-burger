23757f4c04f4b5affb8c141420d512e780b8c09a
Ryan Parker <ryansparker@gmail.com>
2020-05-27 23:11:47 -0400

    Fix for stuff

--- models/burgers.js
+++ models/burgers.js
@@ -2,13 +2,13 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function() {
-      return orm.all("Burgers") 
+      return orm.all("burgers") 
     },
     insert: function(object) {
-        return orm.insert("Burgers", object)
+        return orm.insert("burgers", object)
     },
     update: function(id, object) {
-        return orm.update('Burgers', id, object) 
+        return orm.update("burgers", id, object) 
     }
   };
 
